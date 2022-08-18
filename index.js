9597bcec4a47da16d11a43c8ccb564c5e79e72fa
Zenexya <101019279+Zenexya@users.noreply.github.com>
2022-08-18 22:29:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const express = require('mysql');
+const app = express();
+
+app.get('/', function(req, res) {
+    res.send("Currently unavailable");
 })
-app.listen(process.env.PORT || 3000)
+
+app.listen(3000, function() {
+    console.log("[*]: Server started");
+})
