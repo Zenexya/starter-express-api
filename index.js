54fff41c7f5ff59e2c035fe59ecce8b282a2a4e3
Zenexya <101019279+Zenexya@users.noreply.github.com>
2022-08-18 22:49:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.get('/', function(req, res) {
     res.send("Currently unavailable");
 })
 
+app.get('/admin', function(req, res) {
+    res.send("Admin panel currently unavailable");
+})
+
 app.listen(process.env.PORT || 3000);
